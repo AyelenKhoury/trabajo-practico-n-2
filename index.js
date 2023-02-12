4cc6a8c936c4580b6c82e58350cfa151f0bc7535
Ayelen Khoury <ayelenkhoury@gmail.com>
2023-02-12 20:24:41 -0300

    Completé hasta el punto n°4

---
+++ index.js
@@ -0,0 +1,60 @@
+// Crear 5 variables de tipo number y evaluar si cada una de esas variables es par o impar, en caso de ser par, imprimir “la variable es par”, en caso de ser impar el mensaje deberá decir, “la variable es impar”.
+
+let num1 = 126;
+let num2 = 51;
+let num3 = 254;
+let num4 = 185;
+let num5 = 44;
+
+if (num1 % 2 === 0) {
+  console.log("La variable es par");
+} else {
+  console.log("La variable es impar");
+}
+
+if (num2 % 2 === 0) {
+  console.log("La variable es par");
+} else {
+  console.log("La variable es impar");
+}
+
+if (num3 % 2 === 0) {
+  console.log("La variable es par");
+} else {
+  console.log("La variable es impar");
+}
+
+if (num4 % 2 === 0) {
+  console.log("La variable es par");
+} else {
+  console.log("La variable es impar");
+}
+
+if (num5 % 2 === 0) {
+  console.log("La variable es par");
+} else {
+  console.log("La variable es impar");
+}
+
+
+// Un alumno de programación está creando su primera aplicación web. Crear una variable cuyo valor sea la etiqueta h1 de html con el mensaje “Hola Mundo” en el medio e imprimirla en la consola.
+
+let etiquetaH1 = "<h1>Hola Mundo</h1>";
+console.log(etiquetaH1);
+
+
+// El profesor le pide al alumno que agregue al h1 anterior un poco de estilos, crear una variable cuyo valor sea h1 { color: red; } e imprimirla en consola.
+
+etiquetaEstilos = "h1 {color: red;}";
+console.log(etiquetaEstilos);
+
+
+// Dibujar en la consola una pirámide. 
+// Es una piramide de costado :D
+
+let rref = "<|";
+console.log(rref);
+
+
+
+
