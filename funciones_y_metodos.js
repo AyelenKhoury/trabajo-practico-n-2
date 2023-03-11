56cb1bc54f43aa5fe401e792275152cf31c182ce
Ayelen Khoury <ayelenkhoury@gmail.com>
2023-03-11 09:54:58 -0300

    ejercicios funciones y metodos

---
+++ funciones_y_metodos.js
@@ -0,0 +1,17 @@
+// CALCULADORA ---> Por prompt solicitar la operacion, los valores e indicar con un alert el resultado.
+//      -> Utilizar funciones para las operaciones
+//      -> Utilizar una funcion para enviar el alert con el resultado
+
+function Suma (a, b) {
+    return a + b
+}
+
+// let resultado = Suma (10, 80)
+// console.log(resultado);
+
+
+
+let operacion = "";
+const mensaje = "Por favor, indique que operacion desea realizar";
+
+operacion = prompt (mensaje)
---
+++ whileDoWhile.js
@@ -0,0 +1,8 @@
+// Primeros lugares en concurso de programación
+
+const primerosLugares = ["Ana", "Oswaldo", "Raúl", "Celia", "María", "Antonio"];
+
+for (let index = 0; index < primerosLugares.length; index++) {
+  const element = primerosLugares[index];
+  console.log(element);
+}
